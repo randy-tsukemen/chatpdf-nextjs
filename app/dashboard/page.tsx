31232a76245b962a23b3d5f38ab09b97c6d03ba0
Randy, Huang <gg875437@gmail.com>
2024-08-28 22:57:29 +0900

    add uploader

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,16 @@
-function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+import Documents from "@/components/Documents";
+
+export const dynamic = "force-dynamic";
+
+function Dashboard() {
+  return (
+    <div className="h-full max-w-7xl mx-auto">
+      <h1 className="text-3xl p-5 bg-gray-100 font-extralight text-indigo-600">
+        My Documents
+      </h1>
+      <Documents />
+    </div>
+  );
 }
 
-export default DashboardLayout;
+export default Dashboard;
